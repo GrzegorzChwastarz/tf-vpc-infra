a983ab7d63f6a626eb0521a438567722ed696cde
Grzegorz Chwastarz <grzegorz.chwastarz@branchspace.com>
2021-03-29 15:49:30 +0200

    route tables associated with subnets - fix

--- modules/vpc/routing.tf
+++ modules/vpc/routing.tf
@@ -4,8 +4,18 @@ resource "aws_route_table" "private" {
   tags = var.tags
 }
 
+resource "aws_route_table_association" "private_association" {
+  route_table_id = aws_route_table.private.id
+  subnet_id = aws_subnet.private.id
+}
+
 resource "aws_route_table" "sensitive" {
   vpc_id = aws_vpc.this.id
 
   tags = var.tags
+}
+
+resource "aws_route_table_association" "sensitive_association" {
+  route_table_id = aws_route_table.sensitive.id
+  subnet_id = aws_subnet.sensitive.id
 }
