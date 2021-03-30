ef1da510f60a71c6da208044bffffcc044236429
Grzegorz Chwastarz <grzegorz.chwastarz@branchspace.com>
2021-03-30 20:56:58 +0200

    ec2 s3 egress sg fix

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -25,6 +25,17 @@ resource "aws_security_group_rule" "ec2_s3_endpoint_egress" {
   to_port           = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.ec2_migration_tier.id
-  cidr_blocks       = data.aws_prefix_list.s3.cidr_blocks
+  prefix_list_ids   = [var.vpce_s3.prefix_list_id]
 }
+
+resource "aws_security_group_rule" "ec2_ssmmessages_endpoint_egress" {
+  description       = "Allow outbound connection from EC2 instance to ssm/ssmmessages/ec2messages VPC Endpoints"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ec2_migration_tier.id
+  source_security_group_id = var.ssm_sg_id
+}
+
 
