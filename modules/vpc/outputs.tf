1ef9eaa23c36a185a52f7219cd0cd419fa01f78b
Grzegorz Chwastarz <grzegorz.chwastarz@branchspace.com>
2021-03-31 17:13:27 +0200

    vpc outputs adjusted for aurora module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,6 +6,10 @@ output "private_subnet_id" {
   value = aws_subnet.private.id
 }
 
+output "sensitive_subnet_id" {
+  value = aws_subnet.sensitive.id
+}
+
 output "vpce_s3" {
   value = aws_vpc_endpoint.s3
 }
