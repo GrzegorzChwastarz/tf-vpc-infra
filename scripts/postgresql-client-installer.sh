469d3be6c9050674cadec7cfb84fc3eff6369d42
Grzegorz Chwastarz <grzegorz.chwastarz@branchspace.com>
2021-04-02 11:40:18 +0200

    debugging steps removed from scripts

--- scripts/postgresql-client-installer.sh
+++ scripts/postgresql-client-installer.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-pwd
-# Install postgresql
+
+echo "Install postgresql"
 amazon-linux-extras install postgresql10 -y
-pwd
-# Create given number of files
-pwd && echo " before making dir s3"
+
+echo "Create given number of files locally"
 mkdir ./dummy_files && cd dummy_files
-pwd && echo "before creating files"
 for i in {1..${number_of_files}}; do dd if=/dev/urandom bs=1 count=1 of=file$i; done
-pwd && echo " before uplod to s3"
-# Upload files to legecy bucket
-aws s3 sync . s3://${legacy_bucket_name}/image --region ${region}
-pwd && echo " waiting for db"
-# In case Aurora instance won't spin up on time
+
+echo "Upload files as a content of legecy bucket"
+s3sync -tr ${region} . s3://${legacy_bucket_name}/image
+
+echo "Wait In case Aurora instance won't spin up on time"
 aws rds wait \
     db-instance-available \
     --db-instance-identifier ${db_instance_id} \
     --region ${db_region}
-echo $pwd && echo " before call to db"
-# Create table and fill it with random s3 paths
+
+echo "Create table and fill it with random s3 paths that can act as legacy entries"
 psql postgresql://${db_username}:${db_password}@${db_endpoint_url}:5432/${db_name} << EOF
   CREATE TABLE IF NOT EXISTS avatars (
   id SERIAL PRIMARY KEY,
--- scripts/s3-migrator-installer.sh
+++ scripts/s3-migrator-installer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Copy, unprack and install postgresql clietn to for database operations
-echo $PWD && echo $(ls) && echo " before copying from S3"
+echo "Copy, unprack and install postgresql client to for database operations"
 aws s3 cp s3://${artifacts_bucket_name}/s3sync_2.26_Linux_x86_64.tar /usr/bin --region ${region}
-echo $PWD && echo $(ls) && echo " before unpacking"
-tar -xf s3sync_2.26_Linux_x86_64.tar.gz && rm s3sync_2.26_Linux_x86_64.tar.gz
+cd bin && tar -xf s3sync_2.26_Linux_x86_64.tar && rm s3sync_2.26_Linux_x86_64.tar
