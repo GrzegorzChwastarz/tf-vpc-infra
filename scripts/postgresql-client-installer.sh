08eff12c8888d10781d0d498bb2247369494b525
Grzegorz Chwastarz <grzegorz.chwastarz@branchspace.com>
2021-04-02 13:41:27 +0200

    postgre script - aws s3 command replaced with custom tooling

--- scripts/postgresql-client-installer.sh
+++ scripts/postgresql-client-installer.sh
@@ -14,7 +14,7 @@ echo "Wait In case Aurora instance won't spin up on time"
 aws rds wait \
     db-instance-available \
     --db-instance-identifier ${db_instance_id} \
-    --region ${db_region}
+    --region ${region}
 
 echo "Create table and fill it with random s3 paths that can act as legacy entries"
 psql postgresql://${db_username}:${db_password}@${db_endpoint_url}:5432/${db_name} << EOF
